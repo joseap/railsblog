d61b1ade64787139248a3ce0aa0bd1451476aed1
Jose Plata <jose.a.plata@gmail.com>
2015-10-26 23:58:49 -0400

    corrected forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,17 @@ class User < ActiveRecord::Base
 	validates :password, confirmation: true, presence: true
 	validates_presence_of :username, :fname, :lname, :email, :password, :country
 
-	before_validation :ensure_login_has_a_value
+	before_validation :ensure_username_has_a_value
+	before_validation :normalize_username, on: :create
 
 	protected
-	 def ensure_login_has_a_value
-	 	if login.nil?
-	 		self.login = email unless email.blank?
+	 def ensure_username_has_a_value
+	 	if username.nil?
+	 		self.username = email unless email.blank?
 	 	end
 	 end
+
+	 def normalize_username
+      self.username = self.username.downcase.titleize
+    end
 end
